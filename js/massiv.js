3c612883d2aef7899504295176718ac29fb8cfe0
Vlad-705914 <83553237+Vlad-705914@users.noreply.github.com>
2021-11-25 13:25:01 +0200

    оор

--- js/massiv.js
+++ js/massiv.js
@@ -1,226 +1,150 @@
-// const MyFirstStep = 'Java Script';
-// console.log(MyFirstStep);
-// В консоли вернет Java Script
+// const friends = ['Mango', 'Ajax', 'Poly'];
+// const lastIndex = friends.length-1;
 
-// let username;
-// console.log(username);
-// В консоли undefined(неопознано), при const username(ошибка)
+// for (let i = 0; i <= lastIndex; i += 1){ Перебираем от первого добавляем
+//     последнего
 
-// let age = 10;
 
-// age = 15;
-// console.log(age);
-// В консоли 15
 
-// Оператор typeof - определитель значения
+// //     console.log(friends[i]); КАждый елемент итерации
+// // // }
+// // Метод от обратного
+// const logins = ['Brek', 'sTop', 'noN', 'yes'];
+// const loginToFind = 'yes';
 
-//   let userName;
-// console.log(userName);
-// in consol undefined
-// let inputValue = null;
-// console.log(typeof inputValue);
-// in console object
-// const myString = 'Java Script';
-// console.log(typeof myString);
-// in console string
-// const myNumber = 1959;
-// console.log(typeof myNumber);
-// in console number
-// Описательный вывод
-// const name = 'Vlad';
-// console.log('Name', name);
-// is console Name Vlad
-// const message = 'Help';
-// alert(message);
-// Выводит модальное окно
+// const message = logins.includes(loginToFind) ? 'Пользователь найден' :
+//     'Пользователь не найден';
+// console.log(message );
 
-// Получение данных confirm
-// Просим подтвердить согласие клиента на бронь
-// И сохраняем результаты в переменной
-// const isComing = confirm("Please confirm hotel reservation");
-// console.log(isComing);
-// Модальное окно, ок-true, отмена - falsе
-// prompt
-// Спрашиваем имя отеля в котором бы хотел остановиться клиента
-// Сщхраняем в переменную результаты
+// Поиск самого маленького числа в массиве
 
-// const hotelName = prompt ('Please enter disired name hotel');
-// console.log(hotelName);
-// // Выводит модальное окно куда вводим название отеля
-// // Если нет то null
-// Важная особенность prompt в том, что не зависимо что ввел
-// пользователь, всегда вернется строка.То есть, если пользователь
-//  ввел 5,
-//   то вернется не число 5, а строка "5".
+// При условии что числа не повторяются
+// Метод вытягивания из мешочка по монетке
 
-// Приведение к числу
-// const valueA = '5';
-// console.log(Number(valueA));
-// console.log(typeof Number(valueA));
-// in console 5 and number
-// const valueB = 'Hello';
-// console.log(Number(valueB));
-// console.log(typeof Number(valueB));
-// in console NaN and Number
+// const numbers = [51, 18, 13, 24, 85, 19];
+// let smallNumber = numbers[0];// Первая монетка
+// Перебрали, если та монетка что мы вытянули(number) <
+//     тех что мы вытянули раньше, то она самая маленькая
+// for (number of numbers){
+//     if (number < smallNumber) {
+//         smallNumber = number;
+//    } 
+// }
+// console.log(smallNumber);// 13
 
-// Методы Number.parseInt() и Number.parseFloat()
+// самое большое число
 
-// console.log(Number.parseInt('5px'));
-// console.log(Number.parseInt('5d me 12'));
-// console.log(Number.parseInt('12.74 kju 12'));
-// console.log(Number.parseInt('kjlkh'));
-// Парсит целое число в консоли получаем  5, 5, 12, NaN
+// const numbers = [51, 18, 13, 24, 85, 19];
+// let bigNumber = numbers[0];
+ 
+// for (number of numbers) {
+    
+//     if (number > bigNumber) {
+//         bigNumber = number;
+//     }
+// }
+// console.log( bigNumber);
 
-// console.log(Number.parseFloat("5px"));
-// console.log(Number.parseFloat("12qwe74"));
-// console.log(Number.parseFloat("12.46qwe79"));
-// console.log(Number.parseFloat("qweqwe"));
-// //  Парсит целое число  и дробь в консоли получаем  5, 12, 12.46, NaN
+// Напишите скрипт который обьединяет все элементы в в строку
 
-// Проверка на число
+// Элементов может быть произвольное кол - во
+// Пусть элементы массива будут разделены запятой.
 
-// // const validNumber = Number('51'); //true
-// console.log(Number.isNaN(validNumber)); //false
-// const validNumber = Number('hjhgh'); //false
-// console.log(Number.isNaN(validNumber))// true
-// Для всех значений кроме NaN будет false
+// - Сначала через фор
 
-// Сложение чисел с плавающей точкой
+// Потом через join
 
-// При сложении 0.17 и 0.24, две неточности складываются,
-//   получается незначительная,
-//   но всё же ошибка в вычислениях.
-//   console.log(0.17 + 0.24); // 0.41000000000000003)
-//   применим метод toFixed() в скобках на какой дроби остановиться
-// console.log((0.17 + 0.24).toFixed(2));// 0.41
-//     Math
-// // Math.floor(num) - возвращает наибольшее целое число,
-// // меньшее, либо равное указанному
-// console.log(Math.floor(1.7)); // 1
+// const friends = ['Poly', 'Mango', 'Ajax', 'Kiwi'];
+// let newString ;
 
-// // Math.ceil(num) - возвращает наименьшее целое число,
-// // большее, либо равное указанному числу.
-// console.log(Math.ceil(1.2)); // 2
+// // for (friend of friends) {
+// //     newString = friends + (',');
+// //     newString = newString .slice(0, newString.length-1);
+// // }
 
-// // Math.round(num) - возвращает значение числа,
-// // округлённое до ближайшего целого
-// console.log(Math.round(1.2)); // 1
-// console.log(Math.round(1.5)); // 2
+//  Лучше так 
 
-// // Math.max(num1, num2, ...) - возвращает наибольшее число из набора
-// console.log(Math.max(20, 10, 50, 40)); // 50
+// newString = friends.join(' , ');
+// console.log(newString);
 
-// // Math.min(num1, num2, ...) - возвращает наименьшее число из набора
-// console.log(Math.min(20, 10, 50, 40)); // 10
+// Напиши скрипт который заменяет регистр каждого символа в строке
+// на противоположный
 
-// // Math.pow(base, exponent) - возведение в степень
-// console.log(Math.pow(2, 4)); // 16
+// Например, если строка "JavaScript" в 'jAVAsCRIPT'
 
-// // Math.random() - возвращает псевдослучайное число в диапазоне [0, 1)
-// console.log(Math.random()); // случайное число между 0 и 1
-// console.log(Math.random() * (10 - 1) + 1); // псевдослучайное число от 1 до 10
+// const string = "JavaScript";
+// const letters = string.split('');
+// let inverString = '';
+// // // разбиваем строку по разделителю
+// for (const letter of letters) {
+//     // //   проверяем буквы в нижнем регистре
+// }
+// //     if (letter === letter.toLowerCase()) {
+// // //    те которые в нижнем регистре, записать в нижнем
+// //         // console.log(letter);
+// //         inverString += letter.toUpperCase();
+// //         console.log( inverString );
+// //         // если это не выполнилось сделать в большом регистре
+// //     } else {
+// //         inverString += letter.toLowerCase;
+//     // }
+//     // Лучший вариант тернарник
+    
+//     // inverString += letter === letter.toLowerCase() ? letter.toUpperCase() :
+//     //     letter.toLowerCase();Те что в нижнем регистре сделай в
+//     верхнем и наоборот
+// // }
+    // console.log(inverString); //jAVAsCRIPT
 
-// // Строки//
 
-// const message = 'Mango' + 'is' + 'Grapes';
-// console.log(message);// MangoisGrapes
 
-// const total = 1 + 2 + '3';
-// console.log(1 + "2" + 4); // "124"
-// console.log(total);// 33
-// Цифры складываются, строки склеиваются
+// const title = 'Hello my friends and famile';
+// let test = title.toLowerCase().split(' ').join('-');
+// console.log(test);
 
-// Возвращает позицию(индекс) на которой находится первое
-//  совпадение подстроки или - 1, если ничего не найдено.
+// Сначала переводим в нижний регистр, затем split(' ') собирает в
+// строку и на все это вызывается join('-')
 
-//  Метод indexOf()
-
-// const message = "Welcome to Bahamas!";
-// console.log(message.indexOf("to")); // 8
-// console.log(message.indexOf("hello")); // -1
-
-// Метод includes()#
-
-// Проверяет входит ли подстрока в строку, возвращает буль - true если входит и false в противном случае.Регистр символов в строке и подстроке имеет значение, так как например буква "a" не равна букве "А".
-
-// const productName = "Ремонтный дроид";
-
-// console.log(productName.includes("н")); // true
-// console.log(productName.includes("Н")); // false
-// console.log(productName.includes("дроид")); // true
-// console.log(productName.includes("Дроид")); // false
-// console.log(productName.includes("Ремонтный")); // true
-// console.log(productName.includes("ремонтный")); // false
-
-// Методы replace() и replaceAll()#
-// Возвращают новую строку, в которой первое(replace)
-//  или все совпадения(replaceAll) подстроки заменены на 
-//  указанное значение.
-
-// const jsFileName = "script.js";
-// const minifiedJsFileName = jsFileName.replace(".js", ".min.js");
-// console.log(minifiedJsFileName); // "script.min.js"
-
-// const cssFileNames = "styles.css, about.css, portfolio.css";
-// const minifiedCssFileNames = cssFileNames.replaceAll(".css", ".min.css");
-// console.log(minifiedCssFileNames); // "styles.min.css, about.min.css, 
-// portfolio.min.css"
-
-// Методы replace() и replaceAll() (замена)
-// Возвращают новую строку, в которой первое(replace)
-//  или все совпадения(replaceAll)
-//  подстроки заменены на указанное значение.
-// const jsName = 'script.js';
-// const miniFiend = jsName.replace('.js', '.min.js');
-// console.log(miniFiend); // script.min.js
-
-// const jsNames = 'styles.css , about.css, portfolio.css';
-// const miniFiends = jsNames.replace('.css', '.min.css', 'portfolio.css');
-// console.log(miniFiends); // styles.min.css , about.css, portfolio.css
-
-// Метод slice()#
-// Метод строк slice(startIndex, endIndex) используется для создания копии
-//  части или всей строки.Он делает копию элементов строки от startIndex и
-// до, но не включая endIndex и возвращает новую строку, не изменяя оригинал.
-
-// const productName = "Repair droid";
-// console.log(productName.slice(0, 4)); // "Repa"
-// console.log(productName.slice(3, 9)); // "air dr"
-// console.log(productName.slice(0, productName.length)); // "Repair droid"
-// console.log(productName.slice(7, productName.length)); // "droid"
-
-// Циклы
-
-// let country = 0;
-// while (country<10) {
-//     console.log('country:', country);
-//     country += 1;
-
+// const array1 = [1, 3, 5, 8];
+// let total = 0;
+// const array2 = [1, 8, 5, 16, 9];
+// let newArray = array1.concat(array2);
+// for (array of newArray) {
+//     total += array;
 
 // }
+// console.log(total);
+
+// Удаление
+
+// const carts = [
+//     'Карточка 1',
+//     'Карточка 2',
+//     'Карточка 3',
+//     'Карточка 4',
+//     'Карточка 5',
+//     'Карточка 6',
+// ]
+// console.table(carts);// Делаем таблицу
 
 
-// let counter = 0;
+// let cartIndex = 'Карточка 3';
+// cartIndex = carts.indexOf(cartIndex) // Ищем индекс
+// console.log(cartIndex);//2
 
-// while (counter < 10) {
-//   console.log("counter: ", counter);
-//   counter += 1;
-// }
+// let newCars = carts.splice(0, 1);
 
-// let clientCounter = 18;
-// let maxClients = 25;
-// while (clientCounter < maxClients) {
-//     console.log('clientCounter:', clientCounter);
-//     clientCounter += 1;
+// Вставляем 
 
-// }
+// let newCars = carts.splice(0, 0, 'Карточка ');// Добавилась 'Карточка '
+// в самое начало
 
-// let password = '';
-// do {
-//     password = prompt('Введите пароль длиннее 4 символов', '');
-// } while (password.length < 5);
-// console.log('Ввели пароль', password);
+// Обрезаем
 
-for (let i = 0; i <= 20; i = 5){
-    console.log(i);
-}
+// let newCars = carts.splice(1, 2, 'Piton'); // После 1 индекса
+// удалю 2 элемента и всавлю 'Piton'
+
+// console.log(newCars);
+// console.log(carts);
+
+      
--- js/test.js
+++ js/test.js
@@ -1,283 +1,284 @@
-//Переменные
-// const numberOfSibling = 1 + 3;
-// const numberOfCandies = 8;
-// console.log(numberOfSibling / numberOfCandies);
-
-// const secondsInAMinute = 60;
-// const minutesInAnHour = 60;
-// const secondsInAnHour = secondsInAMinute * minutesInAnHour;
-// console.log(secondsInAnHour);
-
-// const hoursInDay = 24;
-// const secondsInADay = secondsInAnHour * hoursInDay;
-// console.log(secondsInADay);
-
-// const daysInAYear = 365;
-// const secondsInAYear = secondsInADay * daysInAYear ;
-// console.log(secondsInAYear);
-
-// const age = 62;
-// ageSeconds = age * secondsInAYear;
-// console.log(ageSeconds);
-
-// // Инкримент / декремент
-// let x = 5;
-
-// console.log(++x) 
-// console.log(++x)
-// console.log(++x)
-// // Увелчивает на 1 при каждой итерации
-
-// // += (плюс-равно) и –= (минус-равно)
-// let value = 10;
-// value += 10;
-// console.log(value);
-
-// let test = 15;
-// test -= 10;
-// console.log(test);
-
-// // let ballons = 100;
-// // ballons *= 2;
-// // console.log(ballons);
-
-// let ballons = 100;
-// ballons /= 2;
-// console.log(ballons);
-
-// // Строки
-
-// let myThing = 5;
-// myThing = 'Это строка';
-// console.log(myThing);
-
-// let numberNine = 9;
-// let stringNine = '9';
-// let total = numberNine + numberNine;
-// console.log(total);
-// // Сумма 18;
-// let total1 = stringNine + numberNine;
-// console.log(total);
-// // Сумма 18;
-
-// let total2 = stringNine + stringNine;
-// console.log(total2)
-// // Сумма 99 - склеивает строк
-
-// let greeting = 'Привет ';
-// let myName = 'Vlad';
-// result = greeting + myName;
-// console.log(result);
-// // Склеются, если не добавить пробел между последней буквой и ';
 
 
-// //  Длинна строки
-
-// let string = 'superCountry';
-// result = 'superCountry'.length;
-// result = string.length;
-// console.log(result);
-// // Узнаем длинну строки = 12, можно задать значению и самой переменной;
-
-// // const numberOne = 2;
-// // const nuberTwo = 3;
-// // total = numberOne * nuberTwo;
-// // console.log(total);
-
-// // Получение отдельного символа строки
-
-// const myName = 'Vlad';
-// result = myName[0];
-// console.log(result);
-
-// const codeWord1 = 'Space';
-// const codeWord2 = 'Between';
-// const codeWord3 = 'Wrong';
-// const codeWord4 = 'Help';
-// result = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1];
-// console.log(result);
-// Получаем слово'pere'
-
-// Получение среза строки (slice)
-// const longString = 'MynameisVladislav';
-// result = longString.slice(0, 6 );
-// console.log(result);
-// Получаем 'Myname'
-
-// Перевод строки в заглавный или строчный регистр
-// const isTest = 'IsTeSt';
-// result = isTest.toUpperCase();
-// console.log(result);
-// Значение стало в верхнем регистре
-
-// const isTest = 'IsTeSt';
-// result = isTest.toLowerCase();
-// console.log(result);
-//  Значение стало в нижнем регистре
+// ==========Number================
 
 
-// const isName = 'BagEveryDay';
-// const yesName = isName.toLowerCase();
-// Перевели все в нижний регистр
-// const firstName = yesName[0];
-// Выделили 1 букву
-// const thingName = firstName.toUpperCase();
-// Переводим ее в верхний регистр
+// let minuteInAnHour = 60;
+// let seconndsInAnHour = 60;
+// let secondInAnHour = minuteInAnHour * seconndsInAnHour;
+// console.log(secondInAnHour);// 3600
 
-// const lastName = yesName.slice(1);
-// Получаем все буквы кроме первой в нижнем регистре
-// result = thingName + lastName;
-// Все объединяем
-// console.log(result);
-// В итоге получили 'Bageveryday';
+// let hoursInDay = 24;
+// let secondInDay = secondInAnHour * hoursInDay;
+// console.log(secondInDay);// часов в сутках 86400
 
-// const isName = 'BagEveryDay';
-// result = isName[0].toUpperCase() + isName.slice(1).toLowerCase();
-// console.log(result);
-// Простой вариант: 1 букву отделяем и перводим в вехний регистре
-// вырезаем все буквы кроме первой и певодим внижний регистре
-// Затем все складываем
+// let daysInYear = 365;
+// let secondInAnYear = secondInDay * daysInYear;
+// console.log(secondInAnYear);// second in Year // 31536000
 
-// Булевы значения
-// 1. Амперсант &&
-// const hadShower = true;
-// const hasBackpack = true;
-// result = hadShower && hasBackpack;
-// console.log(result);
-// В этом случае true
+// let age = 62;
+//  age * secondInAnYear;
+// console.log(age );
 
-// 2. пайп || (Наоборот)
-// const hadShower = false;
-// const hasBackpack = true;
-// result = hadShower || hasBackpack;
-// console.log(result);
-// // В этом случае true
-// 3. !
-// const isWeekend = true;
-// const heedIsShower = !isWeekend;
-// console.log(heedIsShower);
-//  В этом случае false
-// const isWeekend = false;
-// const hadShower = true;
-// const hasApplle = false;
-// const hasOrange  = true;
-// const shouldGoToSchool = !isWeekend && hadShower && (hasApplle ||
-//     hasOrange);
-    
-// console.log(shouldGoToSchool);
-// Ответ true
+// ===============инкремент, декремент================
+// инкремент прибавляет
 
-// Сравнение
-// const height = 150;
-// const heightRestriction = 155;
-// const result = height < heightRestriction;
-// console.log(result);
-// Результат true
+// // let inFives = 0;
+// // ++inFives;
+// // ++inFives;
+// // ++inFives;
+// // console.log(inFives);// 1,2,3 
 
-// const height = 150;
-// const heightRestriction = 150;
-// const result = height >= heightRestriction;
-// console.log(result);
-// Результат true
+// // декремент убавляет
 
-// const age = 11;
-// const accopaniment = true;
-// const control = 12;
+// let inFives = 6;
+// --inFives;
+// --inFives;
+// --inFives;
 
-// const result = age + accopaniment ===  control;
-// console.log(result);
-// Результат true
+// console.log(inFives);// 5,4,3
 
-// undefined and null
+// =========+=, -= =========
 
-// const Name = null;
+// let score = 10;
+// score += 3;
+// console.log(score); // 13
 
-// console.log(Name);
-// Если в значении пусто будет undefined(не присвоено) есл null будет null(ничто)
+// let score = 10;
+// score -= 3;
+// console.log(score);//  7
 
-// Массивы
-// const myTreeDinosaurus = ['Тиранозавр', 'Велоцираптор', 'Стегозавр',
-//     ' Узурпатор', 'Насильник', 'Сволочь'];
-// total = myTreeDinosaurus[2];
-// console.log(total);
-// Выдало Стегозавр(3 элемент)
+// let score = 10;
+// score *= 3;
+// console.log(score);// 30
 
-// Изменение и создание элементов
-// const myTreeDinosaurus = ['Тиранозавр', 'Велоцираптор', 'Стегозавр',
-//     ' Узурпатор', 'Насильник', 'Сволочь'];
-// total = myTreeDinosaurus[3] = 'Узурпатор new';
-// console.log(total);
-// Изменился на 'Узурпатор new'
+// let score = 10;
+// score /= 2;
+// console.log(score);// 5
 
-// const dinosaurus = [];
-// dinosaurus[0] = 'Тиранозавр';
-// dinosaurus[1] = 'Насильник';
-// dinosaurus[4] = 'Велоцираптор';
-// dinosaurus[3] = ' Узурпатор';
-// dinosaurus[55] = 'Mudak';
+// ==========string===========
+
+// let myString = ' 5'+'6'; читается как строка
+// console.log(myString);// 56, Строки склеятся
+
+// сложение строк
+
+// let myStrit = 'Hello';
+// let myMessage = 'Yes';
+// let newStrit = myStrit + myMessage;
+// console.log(newStrit); // HelloYes
+
+// Длинна строки
+
+// let name = 'Smith';
+
+// console.log(name.length);// Длинна строки 5
+
+// let sumName = 'Smith' + 'Jon';
+// console.log(sumName.length);// Длинна строки 8
+
+
+// ===========book====================
+
+// let dinosaurus = ['Dinosavr', 'Тиранозавр', "Бронтозавр", "Ихтиозавр", "Диплодок" ];
+
+// let godinosaurus = dinosaurus[3];
+// dinosaurus[2]='Тиранозавр Jon'
+
 // console.log(dinosaurus);
-// Добавились в соответсвии с номерами, 'Mudak' - последний
 
-// Разные типы данных в одном массиве
-// Длинна массива
-// const maniacs = ['Yako', 'Nik', 'Sofia'];
-// let=maniacs[0];
-// let=maniacs[1];
-// let = maniacs[2];
-// result = maniacs.length;
+// ==================Добавление в массив================
+
+// let dinosaurus = [];
+// dinosaurus[0] = 'Тиранозавр';
+// dinosaurus[2] = 'Dinosavr';
+// dinosaurus[4] =  "Бронтозавр";
+// console.log(dinosaurus);//(5) ['Тиранозавр', empty, 'Dinosavr', empty, 'Бронтозавр']
+
+
+// let dinosaurus = [];
+// dinosaurus[0] = 'Тиранозавр';
+// dinosaurus[99] = 'Dinosavr';
+
+// console.log(dinosaurus);//(100) ['Тиранозавр', empty × 98, 'Dinosavr']
+
+// ======================Вложение массива в массив============
+// let dinosaursAndNumbers = [3, "динозавры", ["трицератопс", 
+//     "стегозавр", 3627.5], 10];
+// let newDinosaurus = dinosaursAndNumbers[2][0];//мы просим JavaScript обратиться к индексу 2 массива dinosaursAndNumbers, где находится
+// массив ["трицератопс", "стегозавр", 3627.5], и вернуть значение с индексом 0 из этого вложенного массива — это первый элемент,
+// "трицератопс".
+// console.log(newDinosaurus);
+
+// ======================Длина массива===================
+
+// let manjac = ['poly', ' jon', 'silver'];
+// console.log(manjac.length);// 3
+
+// ==================Последний элемент массива======================
+// let manjac = ['poly', ' jon', 'silver'];
+
+// console.log(manjac.length - 1);//2 индекс
+
+// console.log(manjac[manjac.length - 1]);//silver  имя
+
+// ============Какое число(длину) мы получим=================
+
+// let arr = ["Ваня", "Иштван", "Оля"];
+// let newArr = arr;
+// let twoArr = newArr.push("Петя");
+
+// console.log(arr.length);
+// console.log(twoArr);
+
+// let arr = ['Ваня', 'Иштван'];
+// arr.push('Оля');
+// console.log(arr);
+// let newArr = arr.slice(0, 1, 'Петя');
+// console.log(newArr);
+// Заменяем ВАНЮ НЕ петю
+// let arr = ['Ваня', 'Иштван', 'Вася'];
+//  arr.splice(0, 1, 'Петя');
+// console.log(arr);
+
+// let arr = ['Ваня', 'Иштван', 'Вася'];
+// // Удаляем первый елемент
+
+// // Показываем его
+// let newArr = arr.splice(1,1 );
+
+// console.log(newArr);
+// // console.log(newArr);
+
+// Добавили 'Mark', 'Toni'
+
+// let arr = ['Ваня', 'Иштван', 'Вася'];
+// arr.splice(0, 0, 'Mark', 'Toni');
+// console.log(arr);
+
+// Превращаем массив в строку
+
+// let cat = ['Мурка', "Васька", " Митька"];
+// console.log(cat.join());
+
+// Превращаем строку в массив
+
+// let cat = "Мурка,  Митька";
+// console.log(cat.split());
+
+// Вставляем разделитель в массив
+
+// let cat = ["Мурка", "Митька"];
+// console.log(cat.join('-'));
+
+// let oneString = ['Mark', 3, 'Sofia', 5, 'Berg'];
+// console.log(oneString.join('-'));//Mark-3-Sofia-5-Berg
+
+// let landmarks = [];
+// landmarks.push('Мой дом');
+// landmarks.push('Дорога к нему');
+// landmarks.push('Моя бывшая школа');
+// let newName = landmarks.join();
+// let twoName = newName.toLowerCase();
+// threeName = twoName[0];
+// threeName  = threeName.toUpperCase();
+// let firsName = twoName.slice(0)
+// let total = 
+//     console.log(threeName + firsName);
+
+// let test = ['Мой дом', 'Дорога к нему', 'Моя бывшая школа'];
+
+// console.log(test.indexOf['Мой дом']);
+
+// let colors = ["красный", "зеленый", "синий"];
+// // colors.indexOf("синий");
+
+// console.log(colors.join(' i, '));
+
+// ===========Генерация случайного числа======
+
+// console.log(Math.random()*3);
+
+// =============Округляем===================
+
+// console.log(Math.floor(2.7668));// 2
+
+// Получаем случайное число из массива
+
+// let test = ['Djon', 'Kurt',  'Robert'];
+// let newTest = Math.floor(Math.random() * 4);
+// console.log(newTest);
+
+// // Можно записать короче
+// console.log(test[Math.floor(Math.random() * 4)]);
+
+// ================Случайный выбор действий===========
+
+// let phrases = ["Это сделаем сегодня",
+//     " Это сделаем завтра",
+//     "Это можно вообще не делать",
+//     "Это не сделаем никогда"];
+// console.log(phrases[Math.floor(Math.random() *4)]);
+
+// let oneRandom = ["глаз", "нос", "уши"];
+// // 
+// let twoRandom = ["вонучая", "унылая", "дурацкая"];
+// let threeRandom = ["муха", "собака", "конь", "мартышка"];
+// oneRandom = oneRandom[Math.floor(Math.random() * 3)];
+// twoRandom = twoRandom[Math.floor(Math.random() * 3)];
+// threeRandom = threeRandom[Math.floor(Math.random() * threeRandom.length)];
+// // let RandomOnsult = "У тебя "  +  oneRandom  +  "лучше" +  twoRandom +  " "
+// //     +  threeRandom;
+// let RandomOnsult = ["У тебя ", oneRandom, "лучше", twoRandom,
+//     threeRandom + "!!!"].join(" ");
+// console.log(RandomOnsult);
+
+// let oneRandom = ["голова", "шея", "грудь", "ноги"];
+// let twoRandom = ["копыта", "рога", "хвост", "клюв"];
+// let result1 = oneRandom[Math.floor(Math.random() * oneRandom.length)];
+// let result2 = twoRandom[Math.floor(Math.random() * twoRandom.length)];
+// let result = ['У тебя есть ' , result2 , ' еще более чем ', result2 + ' '].join(' -');//У тебя есть  -рога - еще более чем  -рога 
 // console.log(result);
-// Длинна(3)
-//  Свойство(length) 
-// const maniacs = ['Yako', 'Nik', 'Sofia'];
-// let=maniacs[0];
-// let=maniacs[1];
-// let = maniacs[2];
-// result = maniacs[maniacs.length-1];
+
+// let number = [3, 2, 1,];
+// let result = number.join('>');
 // console.log(result);
-// При -1, всегда последний индекс, т.к последний
-// индекс массива всегда на единицу меньше длины этого массива.
 
-//      Вызов метода(puch)
-// const animels = [];
-// animels.push('Cat');
-// animels.push('Dog');
-// animels.push('Bird');
+// 1. Объявление функции multiply
+function multiply() {
+  // Тело функции
+  console.log("Это лог при вызове функции multiply");
+}
 
-// console.log(animels.length);
-// Создает массив из трех элементов(animels)
-// Указывает длинну из 3 элементов(animels.length)
+// 2. Вызовы функции multiply
+multiply(); // 'Это лог при вызове функции multiply'
+multiply(); // 'Это лог при вызове функции multiply'
+multiply();
 
-// Чтобы добавить элемент в начало массива, используйте
-// метод .unshift(элемент)
 
-// const animels = [];
-// animels.push('Cat');
-// animels.push('Dog');
-// animels.push('Bird');
-// animels.unshift('Monkey');
-// console.log(animels);
-// При объвлении animels.unshift('Monkey'), добавился 4 индекс, причем
-// он становится 1;
-// При объвлении animels  'Monkey''Cat''Dog' 'Bird'
 
-// Удаление элементов массива(pop)
-// const animels = ['Cat', 'Dog', 'Bird', 'Monkey' ];
-// lastAnimels = animels.pop();
-// lastAnimels = animels.pop();
-// animels.unshift(lastAnimels);
-// console.log(animels);
-// При lastAnimels = animels.pop(); удаляет по одному индексу сзади
-// При animels.unshift(lastAnimels); добавляет последний удаленный в начало
 
-// Метод animals.shift() только удаляет 1 индекс
 
-// const animels = ['Cat', 'Dog', 'Bird', 'Monkey'];
-// const firstAnimels = animels.shift();
-// animels.unshift(firstAnimels);
-// console.log(animels);
-// При  animels.shift(); удаляет 1 индекс
-// При animels.unshift(firstAnimels); возвращает 1 индекс в начало
-// 59 страница
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
